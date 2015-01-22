8c6bed8f7b5e87700fe81e8848f82cf863e7d1fb
chessmotif <chessmotif@gmail.com>
2015-01-22 16:03:03 +0800

    Added Hikari's shot2, and changed shot1.

--- src/character.js
+++ src/character.js
@@ -13,66 +13,90 @@ function setCharacter(player, charName) {
 // characters
 function hikari_shot1() {
 	var mainshot = new bullet(defaultSpecs(this));
-	var sideshot1 = new bullet(defaultSpecs(this));
-	var sideshot2 = new bullet(defaultSpecs(this));
 
 
 	// main bullet code
-	mainshot.speed = 3;
-	mainshot.size = 7.5;
+	//mainshot.speed = 3;
+	//mainshot.size = 7.5;
 
-	sideshot1.speed = 10;
-	sideshot1.size = 2.5;
-	sideshot1.facing += 2 * Math.PI / 3;
+	for (var i = 0; i < 5; i++) {
+		var sideshot1 = new bullet(defaultSpecs(this));
+		sideshot1.speed = 10 - 2.5*Math.random();
+		sideshot1.size = 2.5;
+		sideshot1.facing += 2 * Math.PI / 3;
 
-	sideshot1.update = function() {
-		if (this.facing - mainshot.facing > - 30 * Math.PI / 180) {
-			if (this.rotationVelocity === undefined)
-				this.rotationVelocity = 5 * Math.PI / 180;
-			if (this.rotationAccel === undefined)
-				this.rotationAccel = 2 * Math.PI / 180;
+		sideshot1.update = function() {
+			if (this.facing - mainshot.facing > (- 30 + 15 * Math.random()) * Math.PI / 180) {
+				if (this.rotationVelocity === undefined)
+					this.rotationVelocity = 5 * Math.PI / 180;
+				if (this.rotationAccel === undefined)
+					this.rotationAccel = 2 * Math.PI / 180;
 
-			this.facing -= this.rotationVelocity;
+				this.facing -= this.rotationVelocity;
+			}
+
+			if (this.destroyed)
+				return;
+
+			this.pos = Point2D.plus(this.pos, Point2D.toXY(this.speed, this.facing));
+
+			if (drawBounds(this.pos))
+				this.destroyed = true;
 		}
 
-		if (this.destroyed)
-			return;
-
-		this.pos = Point2D.plus(this.pos, Point2D.toXY(this.speed, this.facing));
-
-		if (drawBounds(this.pos))
-			this.destroyed = true;
+		this.bullets.push(sideshot1);
 	}
 
-	sideshot2.speed = 10;
-	sideshot2.size = 2.5;
-	sideshot2.facing -= 2 * Math.PI / 3;
+	for (var i = 0; i < 5; i++) {
+		var sideshot2 = new bullet(defaultSpecs(this));
+		sideshot2.speed = 10 - 2.5*Math.random();
+		sideshot2.size = 2.5;
+		sideshot2.facing -= 2 * Math.PI / 3;
 
-	sideshot2.update = function() {
-		if (this.facing - mainshot.facing < 30 * Math.PI / 180) {
-			if (this.rotationVelocity === undefined)
-				this.rotationVelocity = 5 * Math.PI / 180;
-			if (this.rotationAccel === undefined)
-				this.rotationAccel = 2 * Math.PI / 180;
+		sideshot2.update = function() {
+			if (this.facing - mainshot.facing < (30 - 15 * Math.random()) * Math.PI / 180) {
+				if (this.rotationVelocity === undefined)
+					this.rotationVelocity = 5 * Math.PI / 180;
+				if (this.rotationAccel === undefined)
+					this.rotationAccel = 2 * Math.PI / 180;
 
-			this.facing += this.rotationVelocity;
+				this.facing += this.rotationVelocity;
+			}
+
+			if (this.destroyed)
+				return;
+
+			this.pos = Point2D.plus(this.pos, Point2D.toXY(this.speed, this.facing));
+
+			if (drawBounds(this.pos))
+				this.destroyed = true;
 		}
-
-		if (this.destroyed)
-			return;
-
-		this.pos = Point2D.plus(this.pos, Point2D.toXY(this.speed, this.facing));
-
-		if (drawBounds(this.pos))
-			this.destroyed = true;
+		this.bullets.push(sideshot2);
 	}
-
-
-	this.bullets.push(mainshot, sideshot1, sideshot2);
 }
 
 function hikari_shot2() {
-	this.bullets.push(new bullet(defaultSpecs(this)));
+	var mainshot = new bullet(defaultSpecs(this));
+
+	mainshot.speed = 1.5;
+	mainshot.size = 50;
+	mainshot.enemy = this.enemy;
+
+	mainshot.update = function() {
+		this.size-=0.2;
+		if (this.size <= 1)
+			this.destroyed = true;
+
+		if (this.destroyed)
+			return;
+
+		this.pos = Point2D.plus(this.pos, Point2D.norm(Point2D.minus(this.enemy.pos, this.pos),this.speed));
+
+		if (drawBounds(this.pos))
+			this.destroyed = true;
+	}
+
+	this.bullets.push(mainshot);
 }
 
 function default_shot() {
